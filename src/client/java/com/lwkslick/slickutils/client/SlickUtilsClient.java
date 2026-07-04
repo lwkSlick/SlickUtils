5cb45bb6f4dabff633193c7a525ee48166efbbfd
lwkSlick <276837512+lwkSlick@users.noreply.github.com>
2026-07-04 14:11:32 +0530

    1.0.0.0
    initial commit

---
+++ src/client/java/com/lwkslick/slickutils/client/SlickUtilsClient.java
@@ -0,0 +1,10 @@
+package com.lwkslick.slickutils.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlickUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
